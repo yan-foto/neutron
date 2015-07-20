031c17976098bff01d22e6708fe6227b3ea54f4b
Yan Foto <yan.foto@quaintous.com>
2015-07-20 22:25:53 +0200

    Add JS linting (similar to MDL project)

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,18 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+
+gulp.task('jshint', function() {
+  return gulp.src(['src/**/*.js' , 'gulpfile.js'])
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src(['src/**/*.js' , 'gulpfile.js'])
+    .pipe($.jscs())
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('lint', ['jshint', 'jscs']);
