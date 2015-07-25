dbecb0e858926c48af4b14caab0b9743f0fceb98
Yan Foto <yan.foto@quaintous.com>
2015-07-25 19:48:16 +0200

    Add BrowserSync support

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,45 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var mainBowerFiles = require('main-bower-files');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var path = require('path');
+var fs = require('fs');
+
+var sources = {
+  scripts: 'src/**/*.js',
+  views: 'src/**/*.jade',
+  styles: 'src/**/*.{scss,css}',
+  bower: 'bower.json'
+};
 
 gulp.task('jshint', function() {
-  return gulp.src(['src/**/*.js' , 'gulpfile.js'])
+  return gulp.src([sources.scripts , 'gulpfile.js'])
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('jscs', function() {
-  return gulp.src(['src/**/*.js' , 'gulpfile.js'])
+  return gulp.src([sources.scripts , 'gulpfile.js'])
     .pipe($.jscs())
     .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('babel', function() {
-  return gulp.src('src/**/*.js')
+  return gulp.src(sources.scripts)
     .pipe($.babel())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('jade', function() {
-  return gulp.src('src/**/*.jade')
+  return gulp.src(sources.views)
     .pipe($.jade())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', function() {
-  return gulp.src('src/**/*.{scss,css}')
+  return gulp.src(sources.styles)
     .pipe($.sass().on('error', console.error.bind(console, 'Sass error:')))
     .pipe(gulp.dest('dist'));
 });
@@ -47,15 +57,34 @@ gulp.task('electron-manifest', function() {
 });
 
 gulp.task('bower-js-assets', function() {
-  return gulp.src(mainBowerFiles('**/*.js'))
-    .pipe(gulp.dest(path.join('dist', 'js')));
+  if (fs.existsSync(sources.bower)) {
+    return gulp.src(mainBowerFiles('**/*.js'))
+      .pipe(gulp.dest(path.join('dist', 'js')));
+  }
 });
 
 gulp.task('bower-css-assets', function() {
-  return gulp.src(mainBowerFiles('**/*.css'))
-    .pipe(gulp.dest(path.join('dist', 'css')));
+  if (fs.existsSync(sources.bower)) {
+    return gulp.src(mainBowerFiles('**/*.css'))
+      .pipe(gulp.dest(path.join('dist', 'css')));
+  }
+});
+
+gulp.task('serve', ['build'], function() {
+  browserSync({
+    open: false,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+
+  gulp.watch(sources.scripts, ['scripts', reload]);
+  gulp.watch(sources.styles, ['styles', reload]);
+  gulp.watch(sources.bower, ['bower-assets', reload]);
 });
 
 gulp.task('bower-assets', ['bower-css-assets', 'bower-js-assets']);
 
 gulp.task('lint', ['jshint', 'jscs']);
+
+gulp.task('build', ['bower-assets', 'styles', 'jade', 'babel']);
